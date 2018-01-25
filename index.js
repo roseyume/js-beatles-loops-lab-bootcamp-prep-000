4d2dadbbfd1b026b696d38718a7782e7bf583ce2
Rosie Wang <rosiedwang@yahoo.com>
2018-01-25 21:51:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ function johnLennonFacts(facts){
   }
   return newFacts;
 }
+
+function iLoveTheBeatles(number){
+  var empty = [];
+  do{
+    empty.push("I love the Beatles!");
+    number++;
+  }while(number<15);
+  return empty;
+}
